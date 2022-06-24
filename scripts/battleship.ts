9f9549dda09b5726936e1acbb47ac90a86e4bc72
Tobyha01 <tobyhamilton01@gmail.com>
2022-06-24 15:14:22 +0100

    About to move grid into players class
    Signed-off-by: Tobyha01 <tobyhamilton01@gmail.com>

--- scripts/battleship.ts
+++ scripts/battleship.ts
@@ -1,15 +1,16 @@
-// Game
-//     players
-//         Scores
-//     Grid
-//         cells 2d arrays
+class Player{
+
+    grid:string[][] =[]
+
+}
 
 class Game{
 
     players:[]
-    // grid:[] []
     grid: string[][] =[]  //more correct .. an array of string arrays
     guessesLeft:number 
+    private gameArea:HTMLElement = document.getElementById("gameArea")!
+    // guessSquare:string 
     // score:Scores
 
     private makeGrid(rows:number, columns:number){
@@ -19,27 +20,48 @@ class Game{
             this.grid.push([]) //puts in an empty row
 
             for(let j =0; j<=columns; j++){
-                    this.grid[i].push(".")  //pushes dots into this row array
-                }
+                
+                this.grid[i].push("water")  //pushes dots into this row array
+            
             }
         }
-    private positionShips(shipAmount:number){
-        
-        for(let i = 0; i<=shipAmount; i++){
-        
-            let y = Math.floor(Math.random()*this.grid.length)
-            let x = Math.floor(Math.random()*this.grid[0].length) 
-
-            this.grid[y][x]="s"
-
-            
-
-        }
-
-    
     }
 
-    constructor (rows:number, columns:number, players:string[]){
+    private positionShips(shipAmount:number){
+        
+        for(let i = 0; i<shipAmount; i++){
+            
+            let y = Math.floor(Math.random()*this.grid.length)
+            let x = Math.floor(Math.random()*this.grid[0].length) 
+            
+            for(let i = 0; i<5; i++){            
+                
+                this.grid[y][x+i]="ship"
+
+            }
+            
+        }
+
+    }
+
+    //need to create a public method to check whether if a square is ship or water, and change it as required.
+    // Need to create another square type for when a ship square is chosen.   
+    
+    guessShip(guessesLeft:number, shipsLeft:number, guessSquare:string){
+
+        let row = parseInt(guessSquare[1])-1
+        let column = guessSquare.charCodeAt(0)-65
+        if(this.grid[row][column] == "ship"){
+
+        
+            this.grid[row][column]="hit" 
+            this.renderGrid()
+
+        } 
+
+    }
+    
+    constructor (rows:number, columns:number, players:string[], guessesLeft:number, guessSquare:string){
         this.grid = []
         this.players=[]
         this.guessesLeft = 10
@@ -48,55 +70,44 @@ class Game{
         this.makeGrid(rows, columns)
         this.positionShips(10)
 
-        
-        // for(let i = 0; i<=rows; i++){
-        //     grid.push([])
-        //     for(let j =0; j<=columns; j++){
-        //         grid[i].push(".")
-        //     }
-        // }
-
-        // this.grid [0].push([]) 
-        // this.guessesLeft = guessesLeft
+        // this.guessSquare       
+        this.guessesLeft = guessesLeft
         // this.players = players 
         // this.score = score
     }
     
-       
-    
     renderGrid(){
         
-        // generate html based on values in grid - table -flexgrid
-       // genarates a view of the grid to the player
-        
+        // genarates a view of the grid to the player
+    this.gameArea.innerHTML = ""
+
         for(let i = 0; i<this.grid.length; i++){
 
-            let row = document.createElement("div")
+            const row = document.createElement("div")
+            let square = document.createElement("div")
+            row.appendChild(square)
+            square.innerHTML = i+1+"" 
+            square.className = "label"
             row.className = "gridRow"
-            document.body.appendChild(row)
+            this.gameArea.appendChild(row)
 
             for(let j = 0; j<this.grid[i].length; j++){
-                
             
-            if(this.grid[i] [j] == "."){ //i is rows j are columns
+                let square = document.createElement("div")
+                square.className=this.grid[i] [j]
+                row.appendChild(square)
 
-                    let water = document.createElement("div")    
-                    water.className = "water"
-                    row.appendChild(water)    
-                
-                }    
-                
-                else{
-                    
-                    let square = document.createElement("div")
-                    square.className = "square"
-                    row.appendChild(square)    
-
-                }    
-                
+            }    
             
-            }
-                
+        }
+        const row = document.createElement("div")
+        row.className = "gridRow"
+        this.gameArea.appendChild(row)
+        for(let j = 0; j<=this.grid[0].length; j++){
+            let square = document.createElement("div")
+            square.className = "label"
+            row.appendChild(square)
+            square.innerHTML=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"[j]
         }
 
     }
@@ -117,5 +128,10 @@ class Scores{
     }
 }
 
-const game=new Game(10, 40, ["human","computer"])
+const game=new Game(10, 10, ["human","computer"], 50, "a0")
 game.renderGrid()
+
+function shoot(){
+    //read value from from box
+    game.guessShip(0, 0, (<HTMLInputElement>document.getElementById("shoot")).value)
+}
