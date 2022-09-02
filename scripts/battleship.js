2349888567c2634b1104299223913fdc3ade5c91
Tobyha01 <tobyhamilton01@gmail.com>
2022-09-02 17:07:25 +0100

    Made game 2 player (computer vs player), created 2 separate grids with labelled X/Y axis (1 for player/computer), enabled players grid to show visible ship placements and computers grid to hide ship placements. Added different coloured squares: clear blue for hidden/unchecked/empty squares, bright blue for guessed empty squares, red for hit ship squares and black for ship placement squares. Created a coordinate selection box to enter coordinates of player’s desired guess, and enabled the computer to randomly guess after players turn.

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -1,6 +1,5 @@
-"use strict";
-class Player {
-    constructor(name) {
+var Player = /** @class */ (function () {
+    function Player(name) {
         this.name = name;
         this.grid = []; //more correct .. an array of strings arrays
         this.gameArea = document.getElementById("gamearea").appendChild(document.createElement("div"));
@@ -9,43 +8,43 @@ class Player {
         this.positionShips(4);
         this.renderGrid();
     }
-    makeGrid(rows, columns) {
-        for (let i = 0; i <= rows; i++) {
+    Player.prototype.makeGrid = function (rows, columns) {
+        for (var i = 0; i <= rows; i++) {
             this.grid.push([]); //puts in an empty row
-            for (let j = 0; j <= columns; j++) {
+            for (var j = 0; j <= columns; j++) {
                 this.grid[i].push("water"); //pushes dots into this row array
             }
         }
-    }
-    renderGrid() {
+    };
+    Player.prototype.renderGrid = function () {
         // genarates a view of the grid to the player
         this.gameArea.innerHTML = "";
-        for (let i = 0; i < this.grid.length; i++) {
-            const row = document.createElement("div");
-            let square = document.createElement("div");
-            row.appendChild(square);
+        for (var i = 0; i < this.grid.length; i++) {
+            var row_1 = document.createElement("div");
+            var square = document.createElement("div");
+            row_1.appendChild(square);
             square.innerHTML = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i];
             square.className = "label";
-            row.className = "gridRow";
-            this.gameArea.appendChild(row);
-            for (let j = 0; j < this.grid[i].length; j++) {
-                let square = document.createElement("div");
-                let content = this.grid[i][j];
+            row_1.className = "gridRow";
+            this.gameArea.appendChild(row_1);
+            for (var j = 0; j < this.grid[i].length; j++) {
+                var square_1 = document.createElement("div");
+                var content = this.grid[i][j];
                 if (content == "ship" && this.name.toLowerCase() == "computer") //if computers grid
                  {
-                    square.className = "water";
+                    square_1.className = "water";
                 } //display water not ships 
                 else {
-                    square.className = this.grid[i][j];
+                    square_1.className = this.grid[i][j];
                 }
-                row.appendChild(square);
+                row_1.appendChild(square_1);
             }
         }
-        const row = document.createElement("div");
+        var row = document.createElement("div");
         row.className = "gridRow";
         this.gameArea.appendChild(row);
-        for (let j = 0; j <= this.grid[0].length; j++) {
-            let square = document.createElement("div");
+        for (var j = 0; j <= this.grid[0].length; j++) {
+            var square = document.createElement("div");
             square.className = "numbers";
             row.appendChild(square);
             if (j == 0) {
@@ -55,10 +54,10 @@ class Player {
                 square.innerHTML = j + "";
             }
         }
-    }
-    checkGrid(guessesLeft, shipsLeft, guessSquare) {
-        let column = parseInt(guessSquare[1]) - 1;
-        let row = guessSquare.charCodeAt(0) - 65;
+    };
+    Player.prototype.checkGrid = function (guessesLeft, shipsLeft, guessSquare) {
+        var column = parseInt(guessSquare[1]) - 1;
+        var row = guessSquare.charCodeAt(0) - 65;
         if (this.grid[row][column] == "ship") {
             this.grid[row][column] = "hit";
         }
@@ -66,17 +65,17 @@ class Player {
             this.grid[row][column] = "miss";
         }
         this.renderGrid();
-    }
-    positionShips(shipAmount) {
+    };
+    Player.prototype.positionShips = function (shipAmount) {
         //create a separate for loop for vertical and hrizontal position, which takes half the shipamount each    
-        for (let i = 0; i < shipAmount; i++) {
-            let x = 0;
-            let y = 0;
-            for (let attempt = 0; attempt < 100; attempt++) {
+        for (var i = 0; i < shipAmount; i++) {
+            var x = 0;
+            var y = 0;
+            for (var attempt = 0; attempt < 100; attempt++) {
                 y = Math.floor(Math.random() * (this.grid.length - 4));
                 x = Math.floor(Math.random() * (this.grid[0].length - 1)); //has to be -4 when laying across instead of down 
-                let laid = 0;
-                for (let j = 0; j < 5; j++) {
+                var laid = 0;
+                for (var j = 0; j < 5; j++) {
                     // if(this.grid[y][x+j] = laid/2) //while column has less than half ship amount, x=, y=, grid y+ half ship amount 
                     if (this.grid[y + j][x] === "ship") {
                         break;
@@ -88,21 +87,22 @@ class Player {
                     break;
                 }
             }
-            for (let j = 0; j < 5; j++) {
+            for (var j = 0; j < 5; j++) {
                 this.grid[y + j][x] = "ship";
                 // this.grid[y][x+j] = "ship"
             }
         }
-    }
-}
-class Game {
+    };
+    return Player;
+}());
+var Game = /** @class */ (function () {
     // guessSquare:string 
     // score:Scores
-    constructor(playerNames, guessesLeft, guessSquare) {
+    function Game(playerNames, guessesLeft, guessSquare) {
         this.players = [];
         this.gameArea = document.getElementById("gameArea");
         this.players = [];
-        for (let i = 0; i < playerNames.length; i++) {
+        for (var i = 0; i < playerNames.length; i++) {
             this.players.push(new Player(playerNames[i]));
         }
         this.guessesLeft = 10;
@@ -111,20 +111,21 @@ class Game {
         this.guessesLeft = guessesLeft;
         // this.score = score
     }
-}
-class Scores {
-    constructor(shipsSunk, shipsLeft, yourShipsleft) {
+    return Game;
+}());
+var Scores = /** @class */ (function () {
+    function Scores(shipsSunk, shipsLeft, yourShipsleft) {
         this.shipsSunk = shipsSunk;
         this.shipsLeft = shipsLeft;
         this.yourShipsLeft = yourShipsleft;
     }
-}
-const game = new Game(["human", "computer"], 50, " ");
+    return Scores;
+}());
+var game = new Game(["human", "computer"], 50, " ");
 function shoot() {
     //read value from from box
     game.players[1].checkGrid(0, 0, document.getElementById("shoot").value); //players shot on computers grid
-    let guessRow = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * game.players[0].grid.length)]; //computers shot on player
-    let guessColumn = Math.floor(Math.random() * game.players[0].grid[0].length).toString();
+    var guessRow = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * game.players[0].grid.length)]; //computers shot on player
+    var guessColumn = Math.floor(Math.random() * game.players[0].grid[0].length).toString();
     game.players[0].checkGrid(0, 0, guessRow + guessColumn); //computer guess will be random everytime
 }
-//# sourceMappingURL=battleship.js.map
